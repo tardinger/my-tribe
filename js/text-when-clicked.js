935f5f28153ba4223efb0678dc70214ca0cbc60e
tardinger <145486601+tardinger@users.noreply.github.com>
2024-03-19 16:34:54 -0700

    first push

---
+++ js/text-when-clicked.js
@@ -0,0 +1,8 @@
+function showText(text) {
+    document.getElementById("fish15").innerText = text;
+    document.getElementById("displayTextContainer").style.display = "block";
+  }
+  
+  function hideText() {
+    document.getElementById("displayTextContainer").style.display = "none";
+  }
