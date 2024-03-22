7caa8a2ac761632af0abe7dce752e8b7ba2dbb8e
tardinger <145486601+tardinger@users.noreply.github.com>
2024-03-21 18:06:24 -0700

    75% changes

--- js/text-when-clicked.js
+++ js/text-when-clicked.js
@@ -1,11 +1,38 @@
-function showText(text) {
-    if ( document.getElementById("fish15").innerText == text){
-      document.getElementById("fish15").innerText = "";
-    } else {
-      document.getElementById("fish15").innerText = text;
-    }
+function changeFish() {
+  var fish15 = document.getElementById("fish15");
+  var currentContent = fish15.innerHTML.trim();
+
+  if (currentContent.startsWith("<img")) {
+    // If the current content is an image, switch to text
+    fish15.innerHTML = "Congrats! You found the fish.";
+  } else {
+    // If the current content is text, switch to image
+    fish15.innerHTML = '<img src="img/fishiconG1.gif" width="150px">';
   }
-  
-// function hideText() {
-//     document.getElementById("fish15").innerText = "hello";
-//   }
+}
+
+function changeCharacterOne() {
+  var character = document.getElementById("character1");
+  var currentContent = character.innerHTML.trim();
+
+  if (currentContent.startsWith("<img")) {
+    // If the current content is an image, switch to text
+    character.innerHTML = "character1 text";
+  } else {
+    // If the current content is text, switch to image
+    character.innerHTML = '<img src="img/character1.png">';
+  }
+}
+
+function changeCharacterTwo() {
+  var character = document.getElementById("character2");
+  var currentContent = character.innerHTML.trim();
+
+  if (currentContent.startsWith("<img")) {
+    // If the current content is an image, switch to text
+    character.innerHTML = "character2 text";
+  } else {
+    // If the current content is text, switch to image
+    character.innerHTML = '<img src="img/character2.png">';
+  }
+}
